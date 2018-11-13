24abe04468b70e82dfefccca7c48d66314336902
Pavel Keßler <pavel.kessler@gmx.de>
2018-11-13 23:03:46 +0100

    Added Comments

--- chilco/ProcessManager.cs
+++ chilco/ProcessManager.cs
@@ -13,6 +13,7 @@ namespace chilco
         public long MaxPlaytime;
         public long LeftoverTime;
         public Stopwatch ProcessTime = new Stopwatch();
+
         public ProcessManager(string ExePath)
         {
             this.ExePath = ExePath;
@@ -36,16 +37,27 @@ namespace chilco
             }
         }
 
+        /// <summary>
+        /// puts a .disabled after the filename of the .exe, making it impossible to run
+        /// ex. : firefox.exe --> firefox.exe.disabled
+        /// </summary>
         public void Disable()
         {
             File.Move(ExePath, ExePath + ".disabled");
         }
 
+        /// <summary>
+        /// Saves the time that is left over to a file.
+        /// </summary>
         public void SaveLeftoverTime()
         {
             File.WriteAllText(LogsPath + Path.GetFileName(ExePath) + ".txt", "" + (MaxPlaytime + LeftoverTime - ProcessTime.ElapsedMilliseconds));
         }
 
+        /// <summary>
+        /// checks if the process is currently running
+        /// </summary>
+        /// <returns></returns>
         public bool ExeIsRunning()
         {
             if (Process.GetProcessesByName(Path.GetFileName(ExePath)).Length > 0)
@@ -57,6 +69,9 @@ namespace chilco
                 return false;
             }
         }
+        /// <summary>
+        /// kills the process.
+        /// </summary>
         private void KillProcess()
         {
             foreach (var process in Process.GetProcessesByName(Path.GetFileName(ExePath)))
