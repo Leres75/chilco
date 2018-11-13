1d134af9b627e98014e6d3fdcadcf3fa2c5154a5
Pavel Keßler <pavel.kessler@gmx.de>
2018-11-13 22:26:37 +0100

    Added ProcessManager class

---
+++ chilco/ProcessManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace chilco
+{
+    class ProcessManager
+    {
+        public string ExePath;
+        public string LogPath;
+        public Stopwatch ProcessTime = new Stopwatch();
+        public ProcessManager(string ExePath)
+        {
+            this.ExePath = ExePath;
+        }
+
+        public ProcessManager(string ExePath, string LogPath)
+        {
+            this.ExePath = ExePath;
+            this.LogPath = LogPath;
+        }
+
+        public void Disable()
+        {
+            System.IO.File.Move(ExePath, ExePath + ".disabled");
+        }
+
+        public bool IsRunning()
+        {
+            if (Process.GetProcessesByName(Path.GetFileName(ExePath)).Length > 0)
+            {
+                if (!ProcessTime.IsRunning) ProcessTime.Start();
+                return true;
+            }
+            else
+            {
+                if (ProcessTime.IsRunning) ProcessTime.Stop();
+            }
+            return false;
+        }
+    }
+}
