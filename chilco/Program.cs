924d0fb9be0dda6ae62c626f2c8088032cee2ce8
Gökhan Witteborn <goekhan.witteborn@gmx.de>
2018-11-13 21:45:41 +0100

    Added project setup

---
+++ chilco/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace chilco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
